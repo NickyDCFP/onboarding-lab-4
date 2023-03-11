0952098091704ff59b2df3b354ba2063bb478944
NickyDCFP <aniketnickykriplani@gmail.com>
2023-03-11 17:52:57 -0500

    lab 4 complete

--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,6 +1,48 @@
+#include <cstdint>
+
 #include <catch2/catch_test_macros.hpp>
 #include <VExercise2.h>
 
-TEST_CASE("Exercise 2") {
+TEST_CASE("Test VExercise2") {
+  // Test a variety of inputs
+  uint16_t init = 0;
   VExercise2 model;
+
+  // Cycle the clock a few times with reset asserted
+  for(int i = 0; i < 5; i++) {
+    // Call the Verilog module
+    model.clk = 1;
+    model.reset = 0;
+    model.init = init;
+    model.eval();
+    model.clk = 0;
+    model.reset = 1;
+    model.eval();
+
+    // Check if the output is valid
+    REQUIRE((uint16_t) ~model.out == init);
+    init = rand() % 0xFFFF;
+  }
+
+  // Turn off reset
+  uint16_t state = model.out;
+  model.reset = 0;
+  model.clk = 1;
+  model.eval();
+
+  // Test a variety of inputs
+  for(int i = 0; i < 1000; i++) {
+    // Generate a random input value
+    model.clk = 0;
+    model.eval();
+
+    // Compute the next state of the LFSR
+    uint16_t bit =
+        ((state >> 10) ^ (state >> 8) ^ (state >> 3) ^ (state >> 1)) & 1;
+    state = (state << 1) | bit;
+    // Check if the output is correct
+    REQUIRE(model.out == state);
+    model.clk = 1;
+    model.eval();
+  }
 }
--- dv/exercise4.cpp
+++ dv/exercise4.cpp
@@ -1,6 +1,36 @@
 #include <catch2/catch_test_macros.hpp>
+#include <cstdint>
 #include <VExercise4.h>
 
-TEST_CASE("Exercise 4") {
+uint8_t selector(uint8_t sel, uint8_t alpha, uint8_t beta, uint8_t gamma) {
+  switch(sel) {
+    case 0:
+      return alpha;
+    case 1:
+      return beta;
+    case 2:
+      return gamma;
+    default:
+      return alpha & (beta | gamma);
+  }
+}
+
+TEST_CASE("Decoder") {
   VExercise4 model;
+  model.alpha = 1;
+  model.beta = 2;
+  model.gamma = 3;
+  model.cs = 1;
+
+  for(model.sel = 0; model.sel < 4; ++model.sel) {
+    model.eval();
+    REQUIRE(
+        model.out == selector(model.sel, model.alpha, model.beta, model.gamma));
+  }
+  model.cs = 0;
+  model.eval();
+  for(model.sel = 0; model.sel < 4; ++model.sel) {
+    model.eval();
+    REQUIRE(model.out == 0);
+  }
 }
